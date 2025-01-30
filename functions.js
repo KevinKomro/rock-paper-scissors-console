2d9232cf3e2390eaf44bfff2f733a96e73242bee
Kevin Komro <kkomro1993@gmail.com>
2025-01-29 19:04:14 -0600

    Finalize UI Updates
    functions.js => Refactor, added helper functions
    
    rps.js => Moved reusable code into functions ^^
        - Finished logic for paper/scissors
    
    index.html => Linked stylesheet, added classes
    
    styles.css => Added minimal styling

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 const playRound = (p1Answer) => {
-  return checkChoices(p1Answer, getComputerChoice());
+  return determineWinner(p1Answer, getComputerChoice());
 };
 
 // Determine computer choice utilizing math.random()
@@ -16,7 +16,7 @@ function getComputerChoice() {
 
 // Determine winner, or tie.
 // Win conditions are outlined in the README.
-function checkChoices(answer1, answer2) {
+function determineWinner(answer1, answer2) {
   if (answer1 == "rock") {
     if (answer2 == "scissors") {
       return "player";
@@ -39,3 +39,42 @@ function checkChoices(answer1, answer2) {
   // if no conditions above match, the result is a tie.
   return "tie";
 }
+
+const scoreWinner = (result) => {
+  switch (result) {
+    case "player":
+      scorePlayer();
+      break;
+    case "computer":
+      scoreComputer();
+      break;
+    default:
+      div.textContent = `This round was a tie. Player has a score of ${playerScore}. Computer has a score of ${computerScore}`;
+  }
+};
+
+const scorePlayer = () => {
+  playerScore += 1;
+  if (playerScore === 5) {
+    div.textContent = "Player wins the game!";
+    disableButtons();
+  } else {
+    div.textContent = `Player wins this round. Player has a score of ${playerScore}. Computer has a score of ${computerScore}`;
+  }
+};
+
+const scoreComputer = () => {
+  computerScore += 1;
+  if (computerScore === 5) {
+    div.textContent = "Computer wins the game!";
+    disableButtons();
+  } else {
+    div.textContent = `Computer wins this round. Player has a score of ${playerScore}. Computer has a score of ${computerScore}`;
+  }
+};
+
+const disableButtons = () => {
+  btnRock.disabled = true;
+  btnPaper.disabled = true;
+  btnScissors.disabled = true;
+};
--- rps.js
+++ rps.js
@@ -1,29 +1,25 @@
 const btnRock = document.querySelector(".rock");
 const btnPaper = document.querySelector(".paper");
 const btnScissors = document.querySelector(".scissors");
+const div = document.querySelector(".resultDiv");
 
-let playerScore,
-  computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 btnRock.addEventListener("click", () => {
   p1Answer = "rock";
   let result = playRound(p1Answer);
-  switch (result) {
-    case "player":
-      console.log("Player wins!");
-      break;
-    case "computer":
-      console.log("Computer wins!");
-      break;
-    default: 
-      console.log("It's a Tie!")
-  }
+  scoreWinner(result);
 });
 
 btnPaper.addEventListener("click", () => {
   p1Answer = "paper";
+  let result = playRound(p1Answer);
+  scoreWinner(result);
 });
 
 btnScissors.addEventListener("click", () => {
   p1Answer = "scissors";
+  let result = playRound(p1Answer);
+  scoreWinner(result);
 });
